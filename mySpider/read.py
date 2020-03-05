c4e3f4fde5327676f1f21eb0a79756402cdd506e
503589510YK <503589510@qq.com>
2020-03-05 20:10:35 +0800

    第一次

--- mySpider/read.py
+++ mySpider/read.py
@@ -1,23 +1,25 @@
 import json
 import pymysql
 db = pymysql.connect(
-            "localhost", "root", "password", "spiderurl")
+    "localhost", "root", "yk84732225", "spiderurl")
 cursor = db.cursor()
 
 
-f = open("sudaNews2.json",encoding='UTF-8')               # 返回一个文件对象   
-line = f.readline()               # 调用文件的 readline()方法   
-count=0
-while line:   
-    # print(line)# 在 Python 3 中使用   
+f = open("./text.txt", 'r', encoding='utf-8')
+line = f.readline()
+count = 0
+while line:
+    # print(line)# 在 Python 3 中使用
     line = f.readline()
-    load=json.loads(line)
-    # print(load['father'],load['url'])
-    sql = "INSERT IGNORE INTO json(url,father) VALUE ('%s','%s')" % (
+    if line:
+
+        load = json.loads(line)
+        # print(load['father'],load['url'])
+        sql = "INSERT IGNORE INTO json(url,father) VALUE ('%s','%s')" % (
             load['url'], load['father'])
-    cursor.execute(sql)
-    count=count+1
-    
+        cursor.execute(sql)
+        count = count+1
+        print(count, end='|')
 db.commit()
 db.close()
 f.close()
